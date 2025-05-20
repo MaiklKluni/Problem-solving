15d8e3931b0e304a358360d346556b2a8c31b626
MaiklKluni <cool.misha13579@ya.ru>
2025-05-20 07:26:24 +0300

    Basic_data_structures - update 1

--- Basic_data_structures.py
+++ Basic_data_structures.py
@@ -3,21 +3,21 @@ if __name__ == '__main__':
     # 1st task
 
     a, b, c = 9, 0.5, 5
-    print(round((a * c) ** b, 3))
+    print(f"1st answer: {round((a * c) ** b, 3)}")
 
     # 2st task
 
     a, b, c, d = 9.99, 9.98, 1000, 1000.1
-    print((float(a) > b) and (float(c) != d))
+    print(f"2st answer: {(float(a) > b) and (float(c) != d)}")
 
     # 3st task
 
     a, b = 1234, 5678
     a = (a // 10) % 100
     b = (b // 10) % 100
-    print(a + b)
+    print(f"3st answer: {a + b}")
 
-    # 3st task
+    # 4st task
 
     a, b = 13.42, 42.13
-    print((a // 1) == round(b % 1, 2) * 100 or (b // 1) == round(a % 1, 2) * 100)
+    print(f"4st answer: {(a // 1) == round(b % 1, 2) * 100 or (b // 1) == round(a % 1, 2) * 100}")
